cd3e6f5bad405d51ad43bba7a7d88039692a1439
takaki <cheap_trick_magic@yahoo.co.jp>
2021-09-28 18:41:37 +0900

    比較演算子

---
+++ Comparison_Operator/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 比較演算子
+
+func main() {
+	fmt.Println(1 == 1) // true
+
+	fmt.Println(1 == 2) // false
+
+	fmt.Println(4 <= 8) // true
+
+	fmt.Println(1 >= 8) // false
+
+	fmt.Println(1 < 8) // true
+	fmt.Println(3 > 1) // true
+
+	fmt.Println(true == false) // false
+	fmt.Println(true != false) // true
+}
